d92b1509bbc95f42e29540ba1c1a72ae9d3e8b12
lesha.pros.1998@bk.ru <dark.revo4@mail.ru>
2018-03-30 18:56:58 -0700

    test-final

--- app/blocks/menu/menu.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".burger").on("click", function() {
-        $(".burger__line").toggleClass("burger__line_active");
-        $(".menu").toggleClass("menu_active");
-    });
-});
