242741fdcf34e707934479c18224c97bb06159d0
Kafkae <dark.revo4@mail.ru>
2018-04-10 07:34:28 +0300

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,7 @@ let tl = new TimelineMax
 tl	
 	.fromTo('.logo', 0.5, {y:-200,opacity: 0}, {y: 0,opacity: 1})
 	.fromTo('.burger', 1, {y:-200,opacity: 0}, {y: 0,opacity: 1})
+	.staggerFromTo('.navigation__panel .navigation__elem', 0.8, {opacity:0,x: 30}, {opacity: 1,x: 0}, .5)
 
 
 
